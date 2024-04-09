952783b2f278727a3aa37274bdb37b5288f11c02
emartinez <enri.fc.98@gmail.com>
2024-04-09 21:59:24 +0200

    initial logic and POC for the web app

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<script setup>
+	import facts_json from "@/assets/paco_facts.json"
+	import { ref, onBeforeMount } from 'vue'
+
+	const fact = ref("")
+
+	const getRandomFact = () => {
+		return facts_json[Math.floor(Math.random() * facts_json.length)]
+	}
+
+	const updateRandomFact = () => {
+		fact.value = getRandomFact()
+	}
+
+	onBeforeMount(() => {
+		updateRandomFact()
+	})
+</script>
+
+<template>
+	<div>
+		<h1>Curiosidades Paco</h1>
+		<h2>¿Sabías que Paco..</h2>
+		<h3>{{ fact }}?</h3>
+		<button @click="updateRandomFact()">Nueva curiosidad</button>
+	</div>
+</template>
+
+<style scoped>
+</style>
